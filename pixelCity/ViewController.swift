b4123d349859b49e660f80bfb5c01df9ffd05e22
Justin Lindsay <justinlindsay@Justins-iMac.local>
2017-07-29 00:15:59 -0400

    Initial Commit

---
+++ pixelCity/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pixelCity
+//
+//  Created by Justin Lindsay on 7/29/17.
+//  Copyright © 2017 Justin Lindsay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
