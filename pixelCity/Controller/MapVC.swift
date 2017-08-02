879ee5aea99ddad11e046e6dc221b9f951bf5183
Justin Lindsay <justinlindsay@Justins-iMac.local>
2017-08-01 23:21:13 -0400

    downloaded coreml models

--- pixelCity/Controller/MapVC.swift
+++ pixelCity/Controller/MapVC.swift
@@ -15,13 +15,17 @@ class MapVC: UIViewController, UIGestureRecognizerDelegate {
     // Outlets
     
     @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var mapViewBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var pullUpView: UIView!
     
     // Variables
     
     var locationManager = CLLocationManager()
     let authorizationStatus = CLLocationManager.authorizationStatus()
     let regionRadius: Double = 1000
-    
+    var screenSize = UIScreen.main.bounds
+    var spinner: UIActivityIndicatorView?
+    var progressLbl: UILabel?
     
     // Functions
     
@@ -53,9 +57,41 @@ class MapVC: UIViewController, UIGestureRecognizerDelegate {
         }
     }
     
+    func addSwipe() {
+        
+        let swipe = UISwipeGestureRecognizer(target: self, action: #selector(animateViewDown))
+        swipe.direction = .down
+        pullUpView.addGestureRecognizer(swipe)
+        
+    }
+    
+    func animateViewUp() {
+        
+        mapViewBottomConstraint.constant = 300
+        UIView.animate(withDuration: 0.3) {
+            
+            self.view.layoutIfNeeded()
+            
+        }
+    }
+    
+    func addSpinner() {
+        
+        spinner = UIActivityIndicatorView()
+        spinner?.center = CGPoint(x: (screenSize.width / 2) - ((spinner?.frame.width)! / 2), y: 150)
+        spinner?.activityIndicatorViewStyle = .whiteLarge
+        spinner?.color = #colorLiteral(red: 0.2549019754, green: 0.2745098174, blue: 0.3019607961, alpha: 1)
+        spinner?.startAnimating()
+        pullUpView.addSubview(spinner!)
+        
+    }
+    
     @objc func dropPin(sender: UITapGestureRecognizer) {
         
         removePin()
+        animateViewUp()
+        addSwipe()
+        addSpinner()
         
         let touchPoint = sender.location(in: mapView)
         let touchCoordinate = mapView.convert(touchPoint, toCoordinateFrom: mapView)
@@ -66,6 +102,16 @@ class MapVC: UIViewController, UIGestureRecognizerDelegate {
         let coordinateRegion = MKCoordinateRegionMakeWithDistance(touchCoordinate, regionRadius * 2.0, regionRadius * 2.0)
         mapView.setRegion(coordinateRegion, animated: true)
         
+    }
+    
+    @objc func animateViewDown() {
+        
+        mapViewBottomConstraint.constant = 0
+        UIView.animate(withDuration: 0.3) {
+            
+            self.view.layoutIfNeeded()
+            
+        }
     }
     
     // Actions
